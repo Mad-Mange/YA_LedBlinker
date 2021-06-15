cf17b1df03e689fc433aa1a3a1cf4db63b56e3c3
Mad-Mange <madheli@hotmail.com>
2021-06-15 11:56:01 +0200

    Working solution

--- LedBlinker.py
+++ LedBlinker.py
@@ -9,17 +9,20 @@ class LabKit_Serial():
 
 class BlinkSlider(wx.Frame):
 
-    def __init__(self, *args, **kw): ### Constructor
+    def __init__(self, *args, **kw):
         LabKit_Serial.ser.baudrate = 2000000
         LabKit_Serial.ser.port = 'com3'
         LabKit_Serial.ser.open()
+
+        LabKit_Serial.ser.write("led blink 1000\r\n".encode('utf-8'))
 
         super(BlinkSlider, self).__init__(*args, **kw)
 
         self.InitUI()
     
-    def __del__(self): ### Destructor
+    def __del__(self):
         LabKit_Serial.ser.close()
+        print("Running Destructor!!!!")
 
     def InitUI(self):
 
@@ -27,13 +30,13 @@ class BlinkSlider(wx.Frame):
 
         sizer = wx.GridBagSizer(5, 5)
 
-        sld = wx.Slider(pnl, value=200, minValue=1, maxValue=5,
+        sld = wx.Slider(pnl, value=1, minValue=1, maxValue=5,
                         style=wx.SL_HORIZONTAL)
 
         sld.Bind(wx.EVT_SCROLL, self.OnSliderScroll)
         sizer.Add(sld, pos=(0, 0), flag=wx.ALL|wx.EXPAND, border=25)
 
-        self.txt = wx.StaticText(pnl, label='200')
+        self.txt = wx.StaticText(pnl, label='5')
         sizer.Add(self.txt, pos=(0, 1), flag=wx.TOP|wx.RIGHT, border=25)
 
         sizer.AddGrowableCol(0)
@@ -47,10 +50,8 @@ class BlinkSlider(wx.Frame):
         obj = e.GetEventObject()
         val = obj.GetValue()
 
-        blink_txt = 'led blink' + " " + str(val)
-        blink = 'blink_txt\r\n'
+        blink_txt = 'led blink' + " " + str(val * 1000) + '\r\n'
         LabKit_Serial.ser.write(blink_txt.encode('utf-8'))
-        print(blink_txt)
 
         self.txt.SetLabel(str(val))
 
