cf2435e9bd1bd18a3ed10aed016cef8fcc979b06
Mad-Mange <madheli@hotmail.com>
2021-06-15 10:02:10 +0200

    Added some working code

--- LedBlinker.py
+++ LedBlinker.py
@@ -1,1 +1,16 @@
-## Add some code!!
+import serial
+from time import sleep
+
+ser = serial.Serial()
+ser.baudrate = 2000000
+ser.port = 'com3'
+ser.open()
+
+#"blink = 'led on\r\n'
+blink = 'led blink\r\n'
+
+ser.write(blink.encode('utf-8'))
+
+sleep(10)
+
+ser.close()
